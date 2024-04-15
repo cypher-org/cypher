5b8b172f983d14c2e11050f1c03f88d699a38d8f
Yhya <yhyamobile57@gmail.com>
2024-04-15 10:00:47 +0200

    make the connection part of the bot, and use test token if available

--- main.go
+++ main.go
@@ -1,3 +1,41 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+
+	token := os.Getenv("TOKEN")
+
+	if test_token := os.Getenv("TEST_TOKEN"); len(test_token) != 0 {
+		token = test_token
+	}
+
+	discord, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatalf("Error occured during inititalization: %v", err)
+		os.Exit(1)
+	}
+
+	err = discord.Open()
+	if err != nil {
+		log.Fatalf("Error while opening a websocket connection: %v", err)
+		os.Exit(1)
+	}
+
+	defer discord.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Press Ctrl+C to Stop")
+	<-stop
+
+	log.Println("Stopping..")
+}
